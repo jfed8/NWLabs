33f737a9e2235ee838b869be84dc8435443cac10
jfed8 <jadenfeddock@gmail.com>
2016-11-30 09:36:20 -0700

    Repo Begin

---
+++ NWLabsSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NWLabsSite.Startup))]
+namespace NWLabsSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
