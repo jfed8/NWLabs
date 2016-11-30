1f230f009c7b8d582e7d04e4b23bcfa182a2353e
kyardley <klynt.yardley@gmail.com>
2016-11-30 11:40:51 -0700

    Created Catalogs

---
+++ NWLabsSite/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NWLabsSite.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET:
+        public ActionResult Pharmacology()
+        {
+            return View();
+        }
+
+        // GET:
+        public ActionResult Protocol()
+        {
+            return View();
+        }
+
+        // GET:
+        public ActionResult Quote()
+        {
+            return View();
+        }
+    }
+}
