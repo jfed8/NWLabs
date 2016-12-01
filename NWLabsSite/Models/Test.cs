dc9b813d6bfd02ec4ded6cc628fcae191aba3773
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:50:49 -0700

    test

---
+++ NWLabsSite/Models/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Test")]
+    public class Test
+    {
+        [Key]
+        public int Test_ID { get; set; }
+        public string TestDescription { get; set; }
+        public decimal BasePrice { get; set; }
+    }
+}
