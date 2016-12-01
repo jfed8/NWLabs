2bf6110421efa92c4d6c96426297fdbe2cec0780
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:30:07 -0700

    Q

---
+++ NWLabsSite/Models/Quote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Quotes")]
+    public class Quote
+    {
+        [Key]
+        public int Quote_ID { get; set; }
+        public int Assay_ID { get; set; }
+        public int Compound_ID { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
