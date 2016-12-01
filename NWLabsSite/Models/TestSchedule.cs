caa84dba32bdada9106314da2042036ee5487d7b
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:49:19 -0700

    TS

---
+++ NWLabsSite/Models/TestSchedule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Test_Schedule")]
+    public class TestSchedule
+    {
+        [Key]
+        public int SampleTest_ID { get; set; }
+        public int Sample_ID { get; set; }
+        public int Assay_ID { get; set; }
+        public int Test_ID { get; set; }
+        public string ScheduledDate { get; set; }
+    }
+}
