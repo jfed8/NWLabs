e75dec47fcd3443d6145b49bc3cc0529792ce199
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:55:57 -0700

    F

---
+++ NWLabsSite/Models/Files.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Files")]
+    public class Files
+    {
+        [Key]
+        public int FileNumber { get; set; }
+        public int Result_ID { get; set; }
+        public string FileName { get; set; }
+        public int Emp_ID { get; set; }
+        public decimal HoursWorked { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
