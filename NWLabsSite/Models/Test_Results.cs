86d6d2c2251ebaf74c5eb70cd05a313a60ec4e06
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:52:55 -0700

    TR

---
+++ NWLabsSite/Models/Test_Results.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Test_Results")]
+    public class Test_Results
+    {
+        [Key]
+        public int Result_ID { get; set; }
+        public int SampleTest_ID { get; set; }
+        public string ResultDate { get; set; }
+        public int Emp_ID { get; set; }
+        public decimal HoursWorked { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
