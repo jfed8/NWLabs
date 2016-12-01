98bd8769ae38da4b91e98d2f4db5f3a9b7e7296b
kyardley <klynt.yardley@gmail.com>
2016-11-30 21:34:12 -0700

    Added comments for Catalog

--- NWLabsSite/Controllers/CatalogController.cs
+++ NWLabsSite/Controllers/CatalogController.cs
@@ -14,24 +14,28 @@ namespace NWLabsSite.Controllers
             return View();
         }
 
+        //Method: Pharmacology. It returns the Pharmacology report
         // GET:
         public ActionResult Pharmacology()
         {
             return View();
         }
 
+        //Method: Pharmacology. It returns the Protocol Notebook
         // GET:
         public ActionResult Protocol()
         {
             return View();
         }
 
+        //Method: Quote. Returns the view for user to select available compounds for selected Assay
         // GET:
         [HttpGet]
         public ActionResult Quote()
         {
+            
             ViewBag.Assay = "Lipoxygenase";
-
+            //List of available compounds for specified assay, used for dropdownlist in view
             List<SelectListItem> items = new List<SelectListItem>();
 
             items.Add(new SelectListItem { Text = "Magnesium", Value = "0" });
@@ -49,6 +53,7 @@ namespace NWLabsSite.Controllers
             return View();
         }
 
+        //Method: ConfirmedQuote. Returns the view with estimated price for an assay and a specific compound
         public ActionResult ConfirmedQuote()
         {
             ViewBag.Assay = "Lipoxygenase";
