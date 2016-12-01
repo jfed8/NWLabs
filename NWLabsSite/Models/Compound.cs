593899569a2d1a88f4012740b2bfcdbbec121e6e
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:26:58 -0700

    cp

---
+++ NWLabsSite/Models/Compound.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Compound")]
+    public class Compound
+    {
+        [Key]
+        public int Compound_ID { get; set; }
+        public string CompoundName { get; set; }
+    }
+}
