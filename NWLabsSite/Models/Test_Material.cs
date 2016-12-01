72a1471f1f91154410659c49b7684e05b546be2d
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:57:41 -0700

    TM

---
+++ NWLabsSite/Models/Test_Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Test_Material")]
+    public class Test_Material
+    {
+        public int Test_ID { get; set; }
+        public int Material_ID { get; set; }
+        public int QuantityRequired { get; set; }
+    }
+}
