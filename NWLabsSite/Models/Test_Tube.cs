b6cba14df3b1c464d11b9c25da95828637b9f5d2
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:41:44 -0700

    TT

---
+++ NWLabsSite/Models/Test_Tube.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Test_Tube")]
+    public class Test_Tube
+    {
+        [Key]
+        public int TestTube_ID { get; set; }
+        public int Sample_ID { get; set; }
+        public decimal Concentration { get; set; }
+        public int Test_ID { get; set; }
+    }
+}
