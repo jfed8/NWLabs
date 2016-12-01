31f01da2ae78d335e7604346c385b0ed7eea6826
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:33:21 -0700

    Assay

---
+++ NWLabsSite/Models/Assay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Assay")]
+    public class Assay
+    {
+        [Key]
+        public int Assay_ID { get; set; }
+        public string AssayType { get; set; }
+        public string Protocol { get; set; }
+        public int EstimatedDays { get; set; }
+    }
+}
