128b4a327ab2fa1ffe697a2c65f2b020c5859994
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:38:55 -0700

    AT

---
+++ NWLabsSite/Models/Assay_Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Assay_Test")]
+    public class Assay_Test
+    {
+        public int Assay_ID { get; set; }
+        public int Test_ID { get; set; }
+        public bool IsRequired { get; set; }
+    }
+}
