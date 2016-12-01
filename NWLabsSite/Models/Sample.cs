264b3cecdb1b8f60d7136409af9cd73cc7c6dc08
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 17:45:48 -0700

    S

---
+++ NWLabsSite/Models/Sample.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Sample")]
+    public class Sample
+    {
+        [Key]
+        public int Sample_ID { get; set; }
+        public int LT_Number { get; set; }
+        public int SequenceCode { get; set; }
+        public string Appearance { get; set; }
+        public decimal Weight { get; set; }
+        public decimal MolecularMass { get; set; }
+    }
+}
