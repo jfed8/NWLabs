10b076725841fe8bbf7df490957c572f7ef7b456
matthewlc17 <matthewlc17@gmail.com>
2016-11-30 18:00:07 -0700

    material

---
+++ NWLabsSite/Models/Material.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NWLabsSite.Models
+{
+    [Table("Material")]
+    public class Material
+    {
+        [Key]
+        public int Material_ID { get; set; }
+        public string Description { get; set; }
+        public decimal CostPerUnit { get; set; }
+    }
+}
